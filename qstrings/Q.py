ac5d8c6ac23964be29598e3b7f03e18a218c0247
liquidcarbon <akscrps@gmail.com>
2025-08-06 08:56:40 -0600

    some sqlglot

--- qstrings/Q.py
+++ qstrings/Q.py
@@ -1,2 +1,14 @@
-class Q:
-    pass
+import sqlglot
+
+
+class Q(str):
+    def __new__(cls, s: str, **kwargs):
+        instance = str.__new__(cls, s)
+        try:
+            instance.ast = sqlglot.parse_one(instance)
+            instance.errors = ""
+        except sqlglot.errors.ParseError as e:
+            if kwargs.get("validate"):
+                raise e
+            instance.errors = str(e)
+        return instance
--- tests/test_Q.py
+++ tests/test_Q.py
@@ -1,5 +1,15 @@
+import pytest
 from qstrings import Q
+from sqlglot.errors import ParseError
 
 
-def test_q_class():
-    assert Q()
+def test_parse_error():
+    q = Q("SELE 42")
+    assert q.errors
+    with pytest.raises(ParseError):
+        _ = Q("SELE 42", validate=True)
+
+
+def test_parse_select_42():
+    q = Q("SELECT 42")
+    assert q.ast.from_("table").sql() == "SELECT 42 FROM table"
