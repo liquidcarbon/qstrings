124e7f92f2038f53b7836ffaeb66c4d61d0d5c77
liquidcarbon <akscrps@gmail.com>
2025-08-06 15:41:31 -0600

    Q.format; release

--- qstrings/Q.py
+++ qstrings/Q.py
@@ -1,25 +1,59 @@
+import pathlib
 import sqlglot
+
+from os import PathLike
+from typing import Any, Union
+
+PathType = Union[pathlib.Path, Any]
+StrPath = Union[str, PathLike[str]]
 
 
 class Q(str):
     """Smart query string."""
 
-    def __new__(cls, s: str, **kwargs):
-        instance = str.__new__(cls, s)
+    def __new__(cls, s: str, parser_raises: bool = True, **kwargs):
+        """Create a Q string.
+
+        Args:
+            s (str): the base string.
+        """
+        qstr = str.__new__(cls, s)
         try:
-            instance.ast = sqlglot.parse_one(instance)
-            instance.errors = ""
+            qstr.ast = sqlglot.parse_one(s)
+            qstr.errors = ""
         except sqlglot.errors.ParseError as e:
             if kwargs.get("validate"):
                 raise e
-            instance.errors = str(e)
-        return instance
+            qstr.errors = str(e)
+        return qstr
+
+    @classmethod
+    def format(
+        cls,
+        template: StrPath,
+        file: bool = False,
+        path_type: PathType = pathlib.Path,
+        **kwargs,
+    ):
+        if file:
+            return cls.format_from_file(template, path_type, **kwargs)
+        else:
+            return cls(template.format(**kwargs))
+
+    @classmethod
+    def format_from_file(
+        cls, path: StrPath, path_type: PathType = pathlib.Path, **kwargs
+    ):
+        _path = path_type(path)
+        if not _path.exists():
+            raise FileNotFoundError(f"File not found: {_path}")
+        with _path.open("r") as f:
+            template = f.read()
+        return cls(template.format(**kwargs))
 
 
 def sqlglot_sql_q(ex: sqlglot.expressions.Expression, *args, **kwargs):
-    """
-    Add method to sqlglot's Expression.sql method to return a Q string.
-    """
+    """Variant of sqlglot's Expression.sql that returns a Q string."""
     return Q(ex.sql(*args, **kwargs))
 
 
